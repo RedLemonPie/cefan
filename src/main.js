5d7f11ae536e455f6bccc660cd80c8c3f7b3488d
yry <RedLemonPie@gmail.com>
2018-12-27 07:15:07 +0800

    3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from './router'
+//import Axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
